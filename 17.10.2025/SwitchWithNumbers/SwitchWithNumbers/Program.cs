60fc318a353740210cff6c74de9b6f4d1efb819d
Opilane TTHK <opilane@tthk.ee>
2025-10-17 11:07:33 +0300

    SwitchWithNumbers

---
+++ 17.10.2025/SwitchWithNumbers/SwitchWithNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number 1, 2 või 3");
+
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.Beep();
+                    break;
+
+                case 2:
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+
+                case 3:
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+
+                default:
+                    Console.WriteLine("Vale number!");
+                    break;
+            }
+        }
+    }
+}
