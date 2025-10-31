fe0eefa7c550fe91404779e63429acdab0c6679c
Opilane TTHK <opilane@tthk.ee>
2025-10-31 11:39:23 +0200

    SwitchNumbers

---
+++ 31.10.2025/SwitchNumbers/SwitchNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SwitchNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Sisestasid numbri 10");
+                    break;
+
+                case 20:
+                    Console.WriteLine("Sisestasid numbri 20");
+                    break;
+
+                case 30:
+                    Console.WriteLine("Sisestasid numbri 30");
+                    break;
+
+                default:
+                    Console.WriteLine("Tundmatu number!");
+                    break;
+            }
+        }
+    }
+}
