eaacd3c69a68cafc0617531c227f61ee26747865
Opilane TTHK <opilane@tthk.ee>
2025-10-31 10:49:17 +0200

    SwicthMethodCall

---
+++ 31.10.2025/SwicthMethodCall/SwicthMethodCall/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SwicthMethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car coupe või truck");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Car coupe":
+                    CarCoupe();
+                    break;
+
+                case "Truck":
+                    Truck();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR!!!");
+                    break;
+            }
+        }
+
+        static void CarCoupe()
+        {
+            Console.WriteLine("Sa valisid car coupe");
+        }
+        static void Truck()
+        {
+            Console.WriteLine("Sa valisid truck");
+        }
+    }
+}
