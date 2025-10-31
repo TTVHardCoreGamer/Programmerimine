7c11b474d25908a87d94e1d50a54bff7f5244269
Opilane TTHK <opilane@tthk.ee>
2025-10-31 09:54:55 +0200

    SwitchMultipleCases

---
+++ 31.10.2025/SwitchMultipleCases/SwitchMultipleCases/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SwitchMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta teema");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Intro to C#" or "Variables" or "Data Types":
+                    Console.WriteLine("Basic");
+                    break;
+
+                case "OOP" or "Classes" or "Objects":
+                    Console.WriteLine("Intermediate");
+                    break;
+
+                case "Asynchronous Programming" or "LINQ" or "Delegates":
+                    Console.WriteLine("Advanced");
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown course");
+                    break;
+            }
+        }
+    }
+}
