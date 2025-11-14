86a6ec7178cc73ce7050c98fa58268c4ab95748f
Opilane TTHK <opilane@tthk.ee>
2025-11-14 11:24:03 +0200

    SwitchBetweenNumbers

---
+++ 14.11.2025/SwitchBetweenNumbers/SwitchBetweenNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SwitchBetweenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case int n when (n >= 0 && n <= 100):
+                    Console.WriteLine("Arv on vahemikus 0-100");
+                    break;
+                
+                case int n when (n >= 101 && n <= 200):
+                    Console.WriteLine("Arv on vahemikus 101-200");
+                    break;
+
+                case int n when (n >= 201 && n <= 300):
+                    Console.WriteLine("Arv on vahemikus 201-300");
+                    break;
+
+                default:
+                    Console.WriteLine("Arv ei ole vahemikus 0-300");
+                    break;
+            }
+        }
+    }
+}
