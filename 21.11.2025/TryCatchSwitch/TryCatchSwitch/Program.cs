c1a194975f66e020d1a9881d35f1d5dc330befed
Opilane TTHK <opilane@tthk.ee>
2025-11-21 09:37:09 +0200

    TryCatchSwitch

---
+++ 21.11.2025/TryCatchSwitch/TryCatchSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TryCatchSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try catch Switch");
+
+            //tee switch, kus on ainult kaks case
+            //ja valikud on 1 ja 2
+            //defualt haru ei ole vajalik
+            try
+            {
+                int nr = int.Parse(Console.ReadLine());
+
+                switch (nr)
+                {
+                    case 1:
+                        Console.WriteLine("Valisid variandi 1");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Valisid variandi 2");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta number!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või väike!");
+            }
+        }
+    }
+}
