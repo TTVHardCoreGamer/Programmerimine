4d498477415308ae5507bc526712397cc5f6ac96
Opilane TTHK <opilane@tthk.ee>
2025-11-21 11:41:33 +0200

    WhileDoTriangle

---
+++ 21.11.2025/WhileDoTriangle/WhileDoTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace WhileDoTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmnurk");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+
+            while (i <= n)
+            {
+                int j = 1;
+
+                while (j <= i)
+                {
+                    Console.Write(j + " ");
+                    j++;
+                }
+                Console.WriteLine();
+                i++;
+            }
+        }
+    }
+}
