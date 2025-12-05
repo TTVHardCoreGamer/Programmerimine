88375d9f422faec7fa52db72958e3e5914965443
Opilane TTHK <opilane@tthk.ee>
2025-12-05 10:00:16 +0200

    ForLoopEndless

---
+++ 05.12.2025/ForLoopEndless/ForLoopEndless/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ForLoopEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ushort i = 1;
+
+            for (; ; )
+            {
+                Console.WriteLine("i väärtus on " + i);
+                i++;
+
+                if (i > 10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
+
