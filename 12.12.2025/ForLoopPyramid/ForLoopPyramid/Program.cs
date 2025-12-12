19f142293335154ba97a20fe936b5fa996c6c613
Opilane TTHK <opilane@tthk.ee>
2025-12-12 11:38:50 +0200

    ForLoopRomb

---
+++ 12.12.2025/ForLoopPyramid/ForLoopPyramid/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ForLoopPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Loo romb tärnidest: \n");
+            Console.Write("----------------------");
+            Console.Write("\n\n");
+            Console.WriteLine("Sisesta suurus");
+
+            int i, j, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            for (i = n - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
