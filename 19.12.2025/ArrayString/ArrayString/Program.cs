510f2bac49acaf7baeccc0eba818b57cc65922e2
Opilane TTHK <opilane@tthk.ee>
2025-12-19 11:21:36 +0200

    ArrayString

---
+++ 19.12.2025/ArrayString/ArrayString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ArrayString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Euroopa üks pealinnadest on ";
+            Console.WriteLine("Tuple");
+
+            //näide 1
+            var cities = Tuple.Create("Tallinn", "Pariis", "Rooma", "London", "Helsingi");
+            //tuple all on viis erinevat väärtust ja sellepärast saab antud
+            //juhul 5 itemit sisestada
+            Console.WriteLine(sentence + cities.Item5);
+            Console.WriteLine("------------------------------------");
+
+            //näide 2
+            string[] city = { "Tallinn", "Pariis", "Rooma", "London", "Helsingi" };
+            for (int i = 0; i < city.Length; i++)
+            {
+                Console.WriteLine(sentence + city[i]);
+            }
+            Console.WriteLine("------------------------------------");
+
+            //näide 3
+            Console.WriteLine(sentence + city[2]);
+            Console.WriteLine("------------------------------------");
+
+            //näide 4
+            Random number = new Random();
+            int randomNumber = number.Next(0, 5);
+            Console.WriteLine(sentence + city[randomNumber]);
+            Console.WriteLine("------------------------------------");
+        }
+    }
+}
