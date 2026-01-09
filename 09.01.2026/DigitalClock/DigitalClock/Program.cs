8704788722795261ab1b1ee92f314b7cc8d8f6d1
Opilane TTHK <opilane@tthk.ee>
2026-01-09 09:39:53 +0200

    DigitalClock

---
+++ 09.01.2026/DigitalClock/DigitalClock/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DigitalClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Digital Clock";
+            Console.CursorVisible = false;
+
+            //while (true)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("Digital Clock\n");
+            //    Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            //    Thread.Sleep(1000);
+            //}
+
+            //tsükkel
+            //while (true)
+            //{
+                //tühjendab konsooliakna
+                //Console.Clear();
+
+                //muutuja, millel on arvutiaeg
+                //string time = DateTime.Now.ToString("HH:mm:ss");
+
+                //ekraani laius
+                //int x = (Console.WindowWidth - time.Length) / 2;
+
+                //ekraani pikkus
+                //int y = Console.WindowHeight / 2;
+
+                //kella numbri näitamise koht
+                //Console.SetCursorPosition(x, y);
+
+                //näitab aega läbi muutuja time
+                //Console.Write(time);
+
+                //ootab ühe sekundi
+                //Thread.Sleep(1000);
+            //}
+
+            //for loopiga digitaalne kell
+
+            for (; ; )
+            {
+                Console.Clear();
+                Console.WriteLine("Digital Clock\n");
+                Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
